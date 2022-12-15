f99927605fe3a6bca32b0d8ce36da101037742ec
Alexey Maltsev <axel.user.perm@gmail.com>
2022-12-16 02:07:13 +0300

    Solved day 15

--- src/Day15.kt
+++ src/Day15.kt
@@ -3,33 +3,29 @@ import kotlin.math.abs
 data class Day15Data(val scanner: Point<Long>, val beacon: Point<Long>, val distance: Long)
 
 fun main() {
-    fun getRange(data: Day15Data, row: Long): LongArray? {
-        val xDiff = data.distance - abs(row - data.scanner.y)
-        var start = data.scanner.x - xDiff
-        var end = data.scanner.x + xDiff
-        if (start == data.beacon.x) {
-            start++
-        } else if (end == data.beacon.x) {
-            end--
-        }
-
-        return if (start <= end) longArrayOf(start, end) else null
-    }
-
-    fun part1(input: List<String>, row: Long): Long {
-        val data = input.map {
-            Regex("\\d++").findAll(it).map { it.value.toLong() }.toList()
+    fun List<String>.parse(): List<Day15Data> {
+        return this.map {
+            Regex("-?\\d++").findAll(it).map { it.value.toLong() }.toList()
                 .let { (x1, y1, x2, y2) -> Day15Data(Point(y1, x1), Point(y2, x2), abs(x1 - x2) + abs(y1 - y2)) }
         }
+    }
 
+    fun getRange(data: Day15Data, row: Long): LongArray {
+        val xDiff = data.distance - abs(row - data.scanner.y)
+        val start = data.scanner.x - xDiff
+        val end = data.scanner.x + xDiff
+
+        return longArrayOf(start, end)
+    }
+
+    fun getRanges(data: List<Day15Data>, row: Long): List<LongArray> {
         val ranges = data.asSequence()
             .filter { row in it.scanner.y - it.distance..it.scanner.y + it.distance }
             .map { getRange(it, row) }
-            .filterNotNull()
             .sortedBy { it[0] }
             .toList()
 
-        val merged = ranges.fold(mutableListOf<LongArray>()) { acc, range ->
+        return ranges.fold(mutableListOf()) { acc, range ->
             acc.apply {
                 if (isEmpty() || acc.last()[1] < range[0]) {
                     add(range)
@@ -38,18 +34,50 @@ fun main() {
                 }
             }
         }
-
-        val size = merged.sumOf { abs(it[1] - it[0]) + 1 }
-        return size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part1(data: List<Day15Data>, row: Long): Long {
+        val sum = getRanges(data, row).sumOf { abs(it[1] - it[0]) + 1 }
+        val beacons = data.map { it.beacon }.toSet().count { it.y == row }
+        return sum - beacons
     }
 
-    check(part1(readInput("Day15_test"), 10) == 26L)
-    check(part2(readInput("Day15_test")) == 0)
+    fun calcFreq(x: Long, y: Long): Long = x * 4_000_000 + y
 
-    println(part1(readInput("Day15"), 2_000_000))
-    println(part2(readInput("Day15")))
+    fun part2(data: List<Day15Data>, upperBound: Long): Long {
+        for (row in 0..upperBound) {
+            val ranges = getRanges(data, row)
+            when {
+                ranges.size == 1 && ranges[0][0] <= 0 && ranges[0][1] >= upperBound -> continue
+                ranges.size == 1 && ranges[0][0] > 0 -> {
+                    return calcFreq(0, row)
+                }
+                ranges.size == 1 && ranges[0][1] < upperBound -> {
+                    return calcFreq(upperBound, row)
+                }
+                ranges.size == 2 -> {
+                    return calcFreq(ranges[0][1] + 1, row)
+                }
+                else -> error(
+                    "row:$row, ranges: ${
+                        ranges.joinToString(prefix = "[", postfix = "]") {
+                            it.joinToString(
+                                prefix = "[",
+                                postfix = "]"
+                            )
+                        }
+                    }"
+                )
+            }
+        }
+        error("not found")
+    }
+
+    val testInput = readInput("Day15_test").parse()
+    check(part1(testInput, 10) == 26L)
+    check(part2(testInput, 20) == 56000011L)
+
+    val input = readInput("Day15").parse()
+    println(part1(input, 2_000_000))
+    println(part2(input, 4_000_000))
 }
