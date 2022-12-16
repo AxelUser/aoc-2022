042e559f80b33694afba08b8de320a7072e18c4e
Alexey Maltsev <axel.user.perm@gmail.com>
2022-12-16 14:30:33 +0300

    WIP day 16

---
+++ src/Day16.kt
@@ -0,0 +1,15 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    check(part1(readInput("Day16_test")) == 0)
+    check(part2(readInput("Day16_test")) == 0)
+
+    println(part1(readInput("Day16")))
+    println(part2(readInput("Day16")))
+}
